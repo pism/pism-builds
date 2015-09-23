8e41fb74e2582f58036eab5263c7ba24e497e22f
Constantine Khroulev <ckhroulev@alaska.edu>
2015-09-23 11:24:26 -0800

    EOF has to be at the beginning of a line.

--- build_pism.sh
+++ build_pism.sh
@@ -48,19 +48,19 @@ build_petsc() {
         --with-shared-libraries=1
 
     cat > script.queue << EOF
-    #PBS -S /bin/bash
-    #PBS -l select=1:ncpus=1:model=wes
-    #PBS -l walltime=200
-    #PBS -W group_list=s1560
-    #PBS -m e
+#PBS -S /bin/bash
+#PBS -l select=1:ncpus=1:model=wes
+#PBS -l walltime=200
+#PBS -W group_list=s1560
+#PBS -m e
 
-    . /usr/share/modules/init/bash
-    module load comp-intel/2015.0.090
-    module load mpi-intel/5.0.3.048
-    export PATH="$PATH:."
-    export MPI_GROUP_MAX=64
-    mpiexec -np 1 ./conftest-linux-64bit
-    EOF
+. /usr/share/modules/init/bash
+module load comp-intel/2015.0.090
+module load mpi-intel/5.0.3.048
+export PATH="$PATH:."
+export MPI_GROUP_MAX=64
+mpiexec -np 1 ./conftest-linux-64bit
+EOF
 
     # run conftest in an interactive job and wait for it to complete
     qsub -q devel -I -x ./conftest-$PETSC_ARCH
