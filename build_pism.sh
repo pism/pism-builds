fe74253923cd48d845d48185c2969cec3c46811a
Constantine Khroulev <c.khroulev@gmail.com>
2015-09-24 11:42:46 -0800

    Pull proj.4 sources if cloning fails.

--- build_pism.sh
+++ build_pism.sh
@@ -88,12 +88,11 @@ build_fftw3() {
 
 build_proj4() {
     # download and build PROJ.4
-    mkdir -p $LOCAL_LIB_DIR/sources
-    cd $LOCAL_LIB_DIR/sources
+    mkdir -p $LOCAL_LIB_DIR/sources/proj.4
+    cd $LOCAL_LIB_DIR/sources/proj.4
 
-    git clone --depth 1 -b 4.9.2-maintenance https://github.com/OSGeo/proj.4.git
+    git clone --depth 1 -b 4.9.2-maintenance https://github.com/OSGeo/proj.4.git . || git pull
 
-    cd proj.4
     # remove and re-generate files created by autoconf
     autoreconf --force --install
     ./configure --prefix=$LOCAL_LIB_DIR
