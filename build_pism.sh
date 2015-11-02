89adb108697b212b5a61b57525f5cbaec55ca459
Andy Aschwanden <andy.aschwanden@gmail.com>
2015-11-02 15:33:56 -0800

    script now works

--- build_pism.sh
+++ build_pism.sh
@@ -284,4 +284,4 @@ build_ncview
 T="$(($(date +%s)-T))"
 echo "Time in seconds: ${T}"
 
-printf "Pretty format: %02d:%02d:%02d:%02d\n" "$((T/86400))" "$((T/3600%24))" "$((T/60%60))" "$((T%60))""
+printf "Pretty format: %02d:%02d:%02d:%02d\n" "$((T/86400))" "$((T/3600%24))" "$((T/60%60))" "$((T%60))"
