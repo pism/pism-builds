dbe61f29d5f375416b0d4547e4c1c29daea50de4
Constantine Khroulev <ckhroulev@alaska.edu>
2016-03-02 16:47:53 -0900

    Add functions that build parallel NetCDF.

--- pacman/build_pism.sh
+++ pacman/build_pism.sh
@@ -17,6 +17,41 @@ set -e
 # print commands before executing them
 set -x
 
+build_hdf5() {
+    # download and build HDF5
+
+    mkdir -p $LOCAL_LIB_DIR/sources
+    cd $LOCAL_LIB_DIR/sources
+
+    wget -nc http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.gz
+    tar -xvf hdf5-1.8.16.tar.gz
+
+    cd hdf5-1.8.16
+    CC=mpicc ./configure --enable-parallel --prefix=$LOCAL_LIB_DIR
+
+    make all -j $N
+    make install
+}
+
+build_netcdf() {
+    # download and build netcdf
+
+    mkdir -p $LOCAL_LIB_DIR/sources
+    cd $LOCAL_LIB_DIR/sources
+
+    wget -nc ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.0.tar.gz
+    tar -zxvf netcdf-4.4.0.tar.gz
+
+    cd netcdf-4.4.0
+    CC=mpicc CPPFLAGS=-I$LOCAL_LIB_DIR/include LDFLAGS=-L$LOCAL_LIB_DIR/lib ./configure \
+        --enable-netcdf4 \
+        --disable-dap \
+        --prefix=$LOCAL_LIB_DIR 2>&1 | tee netcdf_configure.log
+
+    make all -j $N 2>&1 | tee netcdf_compile.log
+    make install 2>&1 | tee netcdf_install.log
+}
+
 build_petsc () {
     rm -rf $PETSC_DIR
     mkdir -p $PETSC_DIR
@@ -31,7 +66,7 @@ build_petsc () {
     make all
 }
 
-build_pism() {
+build_pism () {
     mkdir -p $PISM_DIR/sources
     cd $PISM_DIR/sources
 
@@ -42,12 +77,16 @@ build_pism() {
     rm -f CMakeCache.txt
 
     cmake -DCMAKE_INSTALL_PREFIX=$PISM_DIR \
-          -DCMAKE_FIND_ROOT_PATH=$LOCAL_LIB_DIR \
-          -DCMAKE_FIND_ROOT_PATH="$LOCAL_LIB_DIR;$HDF5PARALLEL_ROOT;$NETCDF_ROOT" \
+          -DCMAKE_FIND_ROOT_PATH="$LOCAL_LIB_DIR" \
           -DPism_USE_PARALLEL_NETCDF4=YES \
           -DPism_USE_PROJ4=YES $PISM_DIR/sources
     make -j $N install
 }
 
 build_petsc
+
+build_hdf5
+
+build_netcdf
+
 build_pism
