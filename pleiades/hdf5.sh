358778088a203be8016ded2dd55ac506f45c01b0
Constantine Khrulev <ckhroulev@alaska.edu>
2017-12-04 09:50:25 -0800

    Back to using our own HDF5 and NetCDF.

---
+++ pleiades/hdf5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -x
+set -u
+
+N=8
+
+build_hdf5() {
+    # download and build HDF5
+    mkdir -p $LOCAL_LIB_DIR/sources
+    cd $LOCAL_LIB_DIR/sources
+
+    rm -rf hdf5
+    git clone -b hdf5_1_10_1 --depth 1 https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git
+    cd hdf5
+
+    CC=mpicc ./configure --enable-parallel --prefix=$LOCAL_LIB_DIR/hdf5 2>&1 | tee hdf5_configure.log
+
+    make all -j $N 2>&1 | tee hdf5_compile.log
+    make install 2>&1 | tee hdf5_install.log
+}
+
+build_hdf5
