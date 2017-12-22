99c42baf1f44c38dbe26f5ee7b0b878218e43973
Andy Aschwanden <aaschwanden@alaska.edu>
2017-12-21 17:05:58 -0900

    we need a newer version of matplotlib

--- chinook/install_python_modules.sh
+++ chinook/install_python_modules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 build_ocgis() {
-
+    cd $HOME
+    rm -rf ocgis
     git clone git@github.com:NCPP/ocgis.git
     cd ocgis
-    git checkout remotes/origin/v-2.0.0.dev1
     python setup.py install --user
 }
 
@@ -12,6 +12,6 @@ pip install netCDF4 --user
 pip install jupyter --user
 pip install -Iv pygdal==2.1.0.3 --user
 pip install pyproj --user
-
-
-#build_ocgis()
+pip install matplotlib --user
+pip install cdo --user
+build_ocgis
