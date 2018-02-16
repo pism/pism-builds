480ab3060a0eea32ae123fcbfd77e41bae6995ec
Andy Aschwanden <aaschwanden@alaska.edu>
2018-02-16 12:48:08 -0900

    updated python module

--- chinook/install_python_modules.sh
+++ chinook/install_python_modules.sh
@@ -9,12 +9,15 @@ build_ocgis() {
 }
 
 pip install netCDF4 --user
-pip install jupyter --user
+pip install jupyter[all] --user
+pip install ipython[all] --user
 #pip install -Iv pygdal==2.1.0.3 --user
 pip install pyproj --user
 pip install matplotlib --user
 pip install cdo==1.3.2 --user
 pip install nco --user
 pip install cf_units --user
+pip install Unidecode --user
+pip install pyDOE --user
 
 build_ocgis
