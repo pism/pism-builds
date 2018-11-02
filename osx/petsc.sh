aee0d1272ef65d34fc679afedee481c9bc9de136
Andy Aschwanden <aaschwanden@alaska.edu>
2018-11-01 17:28:37 -0800

    Installing PISM and other stuff on High Sierra and Mojave

--- osx/petsc.sh
+++ osx/petsc.sh
@@ -3,33 +3,47 @@
 set -e
 set -x
 
-prefix=${HOME}/local/petsc
+# Make sure PETSC_DIR and PETSC_ARCH are already set before running this script
+
+echo 'PETSC_DIR = ' ${PETSC_DIR}
+echo 'PETSC_ARCH = ' ${PETSC_ARCH}
+
+FLAGS="-O3"
 
 configure_and_build() {
+
     sixty_four=$1
     debugging=$2
+
+    rm -rf $PETSC_DIR
+    mkdir -p $PETSC_DIR
+    cd $PETSC_DIR
+
+    git clone --depth=1 -b maint https://bitbucket.org/petsc/petsc.git .
+
     python2.7 ./config/configure.py \
         --with-shared-libraries \
         --with-fc=0 \
         --with-debugging=${debugging} \
-        --with-64-bit-indices=${sixty_four}
+        --with-64-bit-indices=${sixty_four} \
+        --COPTFLAGS=$FLAGS CXXOPTFLAGS=$FLAGS
+    
 
     make all
 }
 
-build_petsc() {
-    version=3.10.0
 
-    mkdir -p ${prefix}
-    cd ${prefix}
-    wget -nc http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-${version}.tar.gz
-    tar xzf petsc-${version}.tar.gz
+build_petsc4py() {
+    PETSC4PY_DIR=$HOME/petsc4py
+    
+    rm -rf $PETSC4PY_DIR
+    mkdir -p $PETSC4PY_DIR
+    cd $PETSC4PY_DIR
 
-    cd petsc-${version}
-    export PETSC_DIR=$PWD
-
-    export PETSC_ARCH=opt-32bit
-    configure_and_build 0 0
+    git clone --depth=1 -b maint https://bitbucket.org/petsc/petsc4py .
+    python setup.py install --user
+    
 }
 
-build_petsc
+# build_petsc
+build_petsc4py
