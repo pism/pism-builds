3366af0fbe465da3d4d0e5a89aaa1f3c0ca91fce
Andy Aschwanden <aaschwanden@alaska.edu>
2018-11-05 11:44:48 -0900

    added prerequsites for ubuntu

---
+++ ubuntu/prerequisites.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install cmake libfftw3-dev g++ libgsl0-dev netcdf-bin \
+                     libnetcdf-dev libudunits2-dev cdo cmake-curses-gui \
+                     git nco ncview libproj-dev python-dev python-pyproj \
+                     python-netcdf4 libx11-dev libblas-dev liblapack-dev \
+                     openmpi-bin libopenmpi-dev
