5f40505b04df0f39ef23f856bf9ab0a4a6a3e150
Constantine Khroulev <ckhroulev@alaska.edu>
2019-10-31 12:04:25 -0800

    Fix the ParallelIO build script

--- chinook/parallelio.sh
+++ chinook/parallelio.sh
@@ -9,7 +9,7 @@ build_parallelio() {
     netcdf_prefix=${LOCAL_LIB_DIR}/netcdf
     pnetcdf_prefix=${LOCAL_LIB_DIR}/pnetcdf
 
-    url=git@github.com:NCAR/ParallelIO.git
+    url=https://github.com/NCAR/ParallelIO.git
     build=${LOCAL_LIB_DIR}/sources/parallelio
     prefix=${LOCAL_LIB_DIR}/parallelio
 
@@ -18,19 +18,23 @@ build_parallelio() {
 
     git clone --depth=1 -b master ${url} ${build}/sources
 
+    pushd ${build}/build
+
     export CC=mpicc
 
-    cmake -B ${build}/build -S ${build}/sources \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=${prefix} \
-      -DNetCDF_PATH=${netcdf_prefix} \
-      -DPnetCDF_PATH=${pnetcdf_prefix} \
-      -DPIO_ENABLE_FORTRAN=0 \
-      -DPIO_ENABLE_TIMING=0
-
-    pushd ${build}/build
+    cmake \
+	-DCMAKE_C_FLAGS="-fPIC" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=${prefix} \
+	-DNetCDF_PATH=${netcdf_prefix} \
+	-DPnetCDF_PATH=${pnetcdf_prefix} \
+	-DPIO_ENABLE_FORTRAN=0 \
+	-DPIO_ENABLE_TIMING=0 \
+	${build}/sources
 
     make -j4 install
 
     popd
 }
+
+build_parallelio
