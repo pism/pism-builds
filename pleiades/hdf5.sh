f987ef9a6e5adab61108d5cb732d2ccacbf6ef8f
Andy Aschwanden <andy.aschwanden@gmail.com>
2020-09-02 12:11:21 -0800

    updating software stack on pleiades

--- pleiades/hdf5.sh
+++ pleiades/hdf5.sh
@@ -7,13 +7,20 @@ set -u
 N=8
 
 build_hdf5() {
-    # download and build HDF5
-    mkdir -p $LOCAL_LIB_DIR/sources
-    cd $LOCAL_LIB_DIR/sources
 
-    rm -rf hdf5
-    git clone -b hdf5_1_8_12 --depth 1 https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git
-    cd hdf5
+    version=1.10.5
+    prefix=$LOCAL_LIB_DIR/hdf5
+    build_dir=${LOCAL_LIB_DIR}/sources/hdf5
+    url=https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-${version}/src/hdf5-${version}.tar.gz
+
+    mkdir -p ${build_dir}
+    pushd ${build_dir}
+
+    wget -nc ${url}
+    tar xzf hdf5-${version}.tar.gz
+    
+    pushd hdf5-${version}
+    export CC=mpicc
 
     CC=mpicc ./configure --enable-parallel --disable-static --enable-shared --prefix=$LOCAL_LIB_DIR/hdf5 2>&1 | tee hdf5_configure.log
 
