2eaeaf25f9b24bfdf890ed9eb22c0ec3d3f366f1
Andy Aschwanden <aaschwanden@alaska.edu>
2021-03-02 11:35:52 -0900

    added PETSc build script with external packages

--- chinook/petsc-mumps.sh
+++ chinook/petsc-mumps.sh
@@ -11,23 +11,29 @@ MKL=/usr/local/pkg/numlib/imkl/11.3.3.210-pic-iompi-2016b/mkl/lib/intel64
 optimization_flags="-O3 -axCORE-AVX2 -xSSE4.2 -ipo -fp-model precise"
 
 build_petsc() {
-    rm -rf $PETSC_DIR
-    mkdir -p $PETSC_DIR
+    # rm -rf $PETSC_DIR
+    # mkdir -p $PETSC_DIR
     cd $PETSC_DIR
 
-    git clone --depth=1 -b release https://gitlab.com/petsc/petsc.git .
+    # git clone --depth=1 -b release https://gitlab.com/petsc/petsc.git .
 
     # Note that on Chinook mpicc and mpicxx wrap Intel's C and C++ compilers
     ./config/configure.py \
 	--download-petsc4py \
 	--with-cc=mpicc \
 	--with-cxx=mpicxx \
-	--with-fc=0 \
+        --with-fortran=0 \
+        --with-fc=mpifort \
+        --download-scalapack \
+        --download-mumps \
+        --download-hypre \
+        --download-parmetis \
+        --download-metis \
+        --download-ptscotch \
+        --with-openmp \
 	--CFLAGS="${optimization_flags}" \
 	--known-mpi-shared-libraries=1 \
 	--with-blas-lapack-dir=${MKL} \
-	--with-64-bit-indices=1 \
-        --known-64-bit-blas-indices \
         --with-debugging=0 \
 	--with-valgrind=0 \
 	--with-x=0 \
