49990513ebb309f5d77c2a1512f03765bdc2b19c
Andy Aschwanden <andy.aschwanden@gmail.com>
2022-06-03 13:48:34 -0800

    removing mpi/clang for now

--- osx/prerequisites.sh
+++ osx/prerequisites.sh
@@ -11,27 +11,64 @@ if [ -z "$PORT" ] ; then
     PORT='sudo port'
 fi
 
-python=39
-cc=clang11
+python=310
+clang=13
 
-sudo port -vN install mpich-${cc} \
-     hdf5 +threadsafe +mpich +${cc} \
-     netcdf +mpich +${cc} \
-     cdo +mpich +${cc} +cdi \
-     python${python} \
-     OpenBLAS +native \
-     screen \
-     py${python}-flake8 \
-     py${python}-flake8-pep8-naming \
-     py${python}-numpy +openblas \
-     py${python}-cython \
-     emacs-app \
-     py${python}-build \
-     py${python}-openpyxl \
-     py${python}-xlrd \ 
+$PORT -vN install \
+     mpich \
+     hdf5  \
+     netcdf  \
+     cdo +cdi +grib_api  \
+     gdal +netcdf +hdf5  \
+     ncview \
      git +bash_completion +svn \
-     py${python}-black      py${python}-jupyterlab      py${python}-pip      py${python}-autopep8      py${python}-pyproj      py${python}-scipy      py${python}-nose      py${python}-simplegeneric      py${python}-future      py${python}-netcdf4 +mpich      py${python}-matplotlib      py${python}-seaborn      py${python}-statsmodels      py${python}-pip      py${python}-pandas      py${python}-xarray   py${python}-virtualenv      gdal +netcdf +hdf5 +mpich       ncview py${python}-pyqt5 +webkit qgis3 +mpich \
-     
+     wget \
+     emacs-app-devel \
+     doxygen \
+     aspell aspell-dict-en aspell-dict-de aspell-dict-de-alt \
+     ffmpeg +nonfree \
+     py${python}-nose \
+     py${python}-future \
+     py${python}-black \
+     py${python}-sphinx \
+     py${python}-sphinx_rtd_theme \
+     py${python}-jupyter \
+     py${python}-jupyterlab \
+     py${python}-pip \
+     py${python}-autopep8 \
+     py${python}-pyproj \
+     py${python}-scipy \
+     py${python}-shapely \
+     py${python}-cython \
+     py${python}-netcdf4 \
+     py${python}-matplotlib \
+     py${python}-seaborn \
+     py${python}-statsmodels \
+     py${python}-pip \
+     py${python}-pandas \
+     py${python}-fiona \
+     py${python}-gdal \
+     py${python}-scikit-learn \
+     py${python}-unidecode \
+     pip_select \
+     py${python}-flake8 \
+     py${python}-pylint \
+     py${python}-xarray \
+     py${python}-jupyter \
+     py${python}-jupyterlab \
+     py${python}-virtualenv \
+     py${python}-sympy \
+     py${python}-codestyle \
+     py${python}-autopep8 \
+     py${python}-pytest \
+     py${python}-pathlib2 \
+     py${python}-openpyxl \
+     py${python}-tensorboard \
+     py${python}-tensorboardX \
+     swig-python \
+     py${python}-pyqt5-webengine \
+     py${python}-pyqt5 +scintilla +webkit \
+     qgis3    
 
 
 
@@ -54,7 +91,7 @@ $PORT select --set pylint pylint${python}
 $PORT select --set black black${python}
 
 # # Python modules
-for module in braceexpand cdo nco SALib pyDOE sklearn torch torchvision torchaudio tensorboardX pytorch-lightning gpytorch feather cf-units cf-xarray pint-xarray; do
+for module in braceexpand cdo nco SALib pyDOE sklearn torch torchvision torchaudio tensorboardX pytorch-lightning gpytorch cf-units cf-xarray pint-xarray; do
     python -m pip install $module --user
 done
 
