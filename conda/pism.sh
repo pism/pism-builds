9d8153c0ab4f3f2a05f6c82d06b348a735db8654
Andy Aschwanden <andy.aschwanden@gmail.com>
2024-01-10 11:03:14 -0900

    added files

---
+++ conda/pism.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+optimization_flags="-O3"
+
+# No. of cores for make
+N=8
+PISM_DIR=$HOME/pism-conda
+
+# the default branch is "dev"
+branch=dev
+if [ $# -gt 0 ] ; then  # if user says "pism.sh frontal-melt" then use "frontal-melt" branch
+  branch="$1"
+fi
+
+# the default directory is "pism-conda"
+PISM_DIR=$HOME/pism-conda
+if [ $# -gt 1 ] ; then  # if user says "pism.sh frontal-melt" then use "frontal-melt" branch
+  PISM_DIR="$2"
+fi
+
+
+build_pism() {
+    set -e
+    set -x
+    mkdir -p $PISM_DIR/sources
+    cd $PISM_DIR/sources
+
+    git clone --no-single-branch https://github.com/pism/pism.git . || git pull
+
+    git checkout $branch
+
+    rm -rf build
+    mkdir -p build
+    cd build
+
+    export CC=mpicc
+    export CXX=mpicxx
+    cmake -DCMAKE_CXX_FLAGS="${optimization_flags}" \
+          -DCMAKE_C_FLAGS="${optimization_flags}" \
+          -DCMAKE_INSTALL_PREFIX=$PISM_DIR \
+	  -DPism_BUILD_PYTHON_BINDINGS=ON \
+          -DPism_USE_JANSSON=NO \
+          -DPism_PKG_CONFIG_STATIC=OFF \
+          -DPism_USE_PARALLEL_NETCDF4=NO \
+          -DPism_USE_PROJ=YES \
+          $PISM_DIR/sources
+    make -j $N install
+    set +x
+    set +e
+}
+
+build_pism
