1ff9aaa288609e96a6e2c54d6239f7005f589318
Andy Aschwanden <andy.aschwanden@gmail.com>
2024-09-17 11:39:42 -0800

    Added conda packages to build PISM with YAC

--- conda/install-prerqs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-conda create --name pism
-source activate pism
-#conda install -c conda-forge cmake gsl udunits2 proj openmpi-mpicc openmpi-mpicxx lapack fftw petsc petsc4py ncview nco cdo nose doxygen tbb=2020.2
-conda install -c conda-forge gdal cmake gsl udunits2 proj openmpi-mpicc openmpi-mpicxx lapack fftw ncview nco cdo nose doxygen tbb=2020.2
