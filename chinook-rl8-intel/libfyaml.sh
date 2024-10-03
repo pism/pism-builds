8e73c1d4a2cb4056211a0baf413d9417cf3f489c
Constantine Khrulev <ckhroulev@alaska.edu>
2024-10-03 11:10:01 -0800

    Add scripts building libfyaml, YAXT, and YAC using Intel's compilers

---
+++ chinook-rl8-intel/libfyaml.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf libfyaml
+
+git clone https://github.com/pantoniou/libfyaml.git
+
+cd libfyaml
+
+./bootstrap.sh
+
+./configure --prefix=$LOCAL_LIB_DIR \
+	    CC="mpicc -cc=icx" \
+	    CXX="mpicc -cc=icx"
+
+make all
+
+make install
