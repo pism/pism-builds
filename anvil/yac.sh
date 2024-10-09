362860c96bc714a327366d2d5fcfaf0d3a7e797b
Andy Aschwanden <andy.aschwanden@gmail.com>
2024-10-09 15:46:15 -0400

    work on compiling PISM on Anvil

---
+++ anvil/yac.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -u
+set -e
+set -x
+
+rm -rf yaxt
+rm -rf yac
+
+prefix=$LOCAL_LIB_DIR
+
+yaxt_version=0.11.2
+git clone -b release-${yaxt_version} \
+    https://gitlab.dkrz.de/dkrz-sw/yaxt.git
+
+cd yaxt
+
+autoreconf -i
+
+./configure --prefix=${prefix} \
+            --with-pic \
+            CC="mpicc -cc=icx" \
+            CFLAGS="-O3 -g -march=native" \
+            FC="mpif90 -f90=ifx"
+
+make all && make install
+
+cd -
+
+yac_version=3.3.0
+git clone -b release-${yac_version} \
+    https://gitlab.dkrz.de/dkrz-sw/yac.git
+
+cd yac
+
+autoreconf -i
+
+./configure --prefix=${prefix} \
+            --with-yaxt-root=${prefix} \
+            --with-fyaml-root=${prefix} \
+            --disable-netcdf \
+            --disable-examples \
+            --disable-tools \
+            --disable-deprecated \
+            --with-pic \
+            CC="mpicc -cc=icx" \
+            CFLAGS="-O3 -g -march=native" \
+            FC="mpif90 -f90=ifx"
+
+make all && make install
