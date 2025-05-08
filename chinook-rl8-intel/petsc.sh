db3094c18e9b6522d46c4e69690dc92021554875
Constantine Khrulev <ckhroulev@alaska.edu>
2025-05-08 09:46:06 -0800

    Update the PETSc build script in chinook-rl8-intel

--- chinook-rl8-intel/petsc.sh
+++ chinook-rl8-intel/petsc.sh
@@ -6,27 +6,27 @@ set -x
 
 echo 'PETSC_DIR = ' ${PETSC_DIR}
 
-optimization_flags="-O3"
+optimization_flags="-O3 -fp-model=precise"
 
 build_petsc() {
     rm -rf $PETSC_DIR
     mkdir -p $PETSC_DIR
     cd $PETSC_DIR
 
-    git clone --depth=1 -b v3.20.6 https://gitlab.com/petsc/petsc.git .
+    git clone --depth=1 -b v3.23.1 https://gitlab.com/petsc/petsc.git .
 
     ./config/configure.py \
         --march=native \
-        --with-cc=mpiicc \
-        --with-fc=0 \
-        --with-cxx=mpiicpc \
+        --with-cc="mpicc -cc=icx" \
+        --with-fc=mpiifort \
+        --with-cxx="mpicc -cc=icx" \
         --CFLAGS="${optimization_flags}" \
         --CXXOPTFLAGS="${optimization_flags}" \
         --known-mpi-shared-libraries=1 \
 	--with-debugging=0 \
         --with-valgrind=0 \
         --with-x=0 \
-	--download-petsc4py \
+	--with-petsc4py \
         --with-ssl=0 \
         --with-batch=1 \
         --with-shared-libraries=1 \
