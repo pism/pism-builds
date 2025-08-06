3d7077877c0a46f7bb0c85efd042883ac59bde93
Andy Aschwanden <andy.aschwanden@gmail.com>
2025-08-06 16:08:59 -0400

    trying to get petsc compiled with MUMPS

--- anvil/petsc.sh
+++ anvil/petsc.sh
@@ -17,10 +17,13 @@ build_petsc() {
 
     ./config/configure.py \
         --march=native \
-        --with-fc=0 \
+        --with-fc="$MPIF90" \
 	--with-cc="$MPICC" \
         --with-cxx="$MPICXX" \
-        --CFLAGS="${optimization_flags}" \
+	--download-cmake \
+	--log-view \
+	--CFLAGS="${optimization_flags}" \
+	--FOPTFLAGS="${optimization_flags} ${opt_flags}" \
         --CXXOPTFLAGS="${optimization_flags} ${opt_flags}" \
         --known-mpi-shared-libraries=1 \
         --with-debugging=0 \
@@ -28,6 +31,8 @@ build_petsc() {
         --with-x=0 \
         --with-ssl=0 \
         --with-batch=1 \
+        --download-mumps \
+        --download-scalapack \
         --with-shared-libraries=1 \
         --with-64-bit-indices \
           | tee petsc-configure.log
